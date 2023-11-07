5a59746687619b01dcdedbcc47dd433fecb0d8a9
zhewei0607 <146732595+zhewei0607@users.noreply.github.com>
2023-11-07 10:11:35 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+      float a,b,c;
+      scanf("%f",&a);
+      if(a<=800)
+      {
+          a=a*(float)9/(float)10;
+          printf("%.1f",a);
+      }
+      else if(a>800 && a<=1500)
+      {
+          a=a*(float)9/(float)10*(float)9/(float)10;
+          printf("%.1f",a);
+      }
+      else
+      {
+          a=a*(float)9/(float)10*(float)7*(float)9/(float)100;
+          printf("%.1f",a);
+      }
+}
 
