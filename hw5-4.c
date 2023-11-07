a503c7afa8b5c61f5df409a76241853588599666
zhewei0607 <146732595+zhewei0607@users.noreply.github.com>
2023-11-07 10:11:58 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+      float hour,min,hourdeg,mindeg,deg;
+      scanf("%f%f",&hour,&min);
+      hourdeg=hour*(float)30+min*(float)5/(float)10;
+      mindeg=min*(float)6;
+      if(mindeg>hourdeg)
+      {
+          if(mindeg-hourdeg>180)
+          {
+               deg=mindeg-hourdeg;
+               deg=360-deg;
+               printf("%.1f",deg);
+          }
+          else
+          {
+               deg=mindeg-hourdeg;
+               printf("%.1f",deg);
+          }
+      }
+      else if(mindeg<hourdeg)
+      {
+          if(hourdeg-mindeg>180)
+          {
+               deg=hourdeg-mindeg;
+               deg=360-deg;
+               printf("%.1f",deg);
+          }
+          else
+          {
+               deg=hourdeg-mindeg;
+               printf("%.1f",deg);
+          }
+      }
+      else
+      {
+          printf("0");
+      }
+}
 
